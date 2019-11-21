6f05454f467760bc625bc769412cafbb11e4e08c
henriqueFutema <henrique.futema01@gmail.com>
2019-11-21 08:18:34 -0300

    Commit inicial

---
+++ observable.js
@@ -0,0 +1,19 @@
+const { Observable } = require('rxjs');
+const breadcrumb = new Array();
+
+const observable = Observable.create(function (observer) {
+  observer.next('Filmes');
+  observer.next('Aventura');
+  observer.next('Espacial');
+  observer.next('Star Wars');
+  setTimeout(() =>{
+      observer.next('aaa')
+  }, 3000)
+  observer.complete();
+});
+
+observable.subscribe({
+  next: link => breadcrumb.push(link),
+  error: err => console.error(`​Mostra se algo der errado: ${err}`),
+  complete: () => console.log(breadcrumb.join("->"))
+});
